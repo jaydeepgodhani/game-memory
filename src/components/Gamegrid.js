78da025622ffba9790df8d600d022f0ecfce94fa
Jaydeep Godhani <godhani.rameshbhai@walmart.com>
2024-05-08 23:41:08 +0530

    working for hide

--- src/components/Gamegrid.js
+++ src/components/Gamegrid.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import Square from './Square';
+import makeBoard from '../utils/makeBoard';
+
+const arrayy = [...Array.from(Array(8).keys()), ...Array.from(Array(8).keys())]
+const values = makeBoard(arrayy);
+console.log(values);
 
 const Gamegrid = () => {
 
-  const [hidden, setHidden] = useState(new Array(16).fill(false));
-  const [clicked, setClicked] = useState(new Array(16).fill(false));
+  const [hidden, setHidden] = useState(Array(16).fill(false));
+  const [clicked, setClicked] = useState(Array(16).fill(false));
 
   function performClick(index) {
     const newClicked = clicked.slice();
@@ -12,31 +17,55 @@ const Gamegrid = () => {
     setClicked(newClicked);
   }
 
+  const found = [];
+  for (let i=0; i<16; i++) {
+    if(clicked[i] === true) {
+      found.push(i);
+    }
+  }
+  console.log(found);
+  if(found.length === 2){
+    if(values[found[0]] === values[found[1]]) {
+      const newClicked = clicked.slice();
+      newClicked[found[0]] = false;
+      newClicked[found[1]] = false;
+      setClicked(newClicked);
+
+      const newHidden = hidden.slice();
+      newHidden[found[0]] = true;
+      newHidden[found[1]] = true;
+      setHidden(newHidden);
+
+    } else {
+      console.log("else...");
+    }
+  }
+
   return (
     <div>
       <div className="flex">
-        <Square hidden={hidden[0]} clicked={clicked[0]} performClick={() => performClick(0)}/>
-        <Square hidden={hidden[1]} clicked={clicked[1]} performClick={() => performClick(1)}/>
-        <Square hidden={hidden[2]} clicked={clicked[2]} performClick={() => performClick(2)}/>
-        <Square hidden={hidden[3]} clicked={clicked[3]} performClick={() => performClick(3)}/>
+        <Square hidden={hidden[0]} clicked={clicked[0]} performClick={() => performClick(0)} backValue={values[0]}/>
+        <Square hidden={hidden[1]} clicked={clicked[1]} performClick={() => performClick(1)} backValue={values[1]}/>
+        <Square hidden={hidden[2]} clicked={clicked[2]} performClick={() => performClick(2)} backValue={values[2]}/>
+        <Square hidden={hidden[3]} clicked={clicked[3]} performClick={() => performClick(3)} backValue={values[3]}/>
       </div>
       <div className="flex">
-        <Square hidden={hidden[4]} clicked={clicked[4]} performClick={() => performClick(4)}/>
-        <Square hidden={hidden[5]} clicked={clicked[5]} performClick={() => performClick(5)}/>
-        <Square hidden={hidden[6]} clicked={clicked[6]} performClick={() => performClick(6)}/>
-        <Square hidden={hidden[7]} clicked={clicked[7]} performClick={() => performClick(7)}/>
+        <Square hidden={hidden[4]} clicked={clicked[4]} performClick={() => performClick(4)} backValue={values[4]}/>
+        <Square hidden={hidden[5]} clicked={clicked[5]} performClick={() => performClick(5)} backValue={values[5]}/>
+        <Square hidden={hidden[6]} clicked={clicked[6]} performClick={() => performClick(6)} backValue={values[6]}/>
+        <Square hidden={hidden[7]} clicked={clicked[7]} performClick={() => performClick(7)} backValue={values[7]}/>
       </div>
       <div className="flex">
-        <Square hidden={hidden[8]} clicked={clicked[8]} performClick={() => performClick(8)}/>
-        <Square hidden={hidden[9]} clicked={clicked[9]} performClick={() => performClick(9)}/>
-        <Square hidden={hidden[10]} clicked={clicked[10]} performClick={() => performClick(10)}/>
-        <Square hidden={hidden[11]} clicked={clicked[11]} performClick={() => performClick(11)}/>
+        <Square hidden={hidden[8]} clicked={clicked[8]} performClick={() => performClick(8)} backValue={values[8]}/>
+        <Square hidden={hidden[9]} clicked={clicked[9]} performClick={() => performClick(9)} backValue={values[9]}/>
+        <Square hidden={hidden[10]} clicked={clicked[10]} performClick={() => performClick(10)} backValue={values[10]}/>
+        <Square hidden={hidden[11]} clicked={clicked[11]} performClick={() => performClick(11)} backValue={values[11]}/>
       </div>
       <div className="flex">
-        <Square hidden={hidden[12]} clicked={clicked[12]} performClick={() => performClick(12)}/>
-        <Square hidden={hidden[13]} clicked={clicked[13]} performClick={() => performClick(13)}/>
-        <Square hidden={hidden[14]} clicked={clicked[14]} performClick={() => performClick(14)}/>
-        <Square hidden={hidden[15]} clicked={clicked[15]} performClick={() => performClick(15)}/>
+        <Square hidden={hidden[12]} clicked={clicked[12]} performClick={() => performClick(12)} backValue={values[12]}/>
+        <Square hidden={hidden[13]} clicked={clicked[13]} performClick={() => performClick(13)} backValue={values[13]}/>
+        <Square hidden={hidden[14]} clicked={clicked[14]} performClick={() => performClick(14)} backValue={values[14]}/>
+        <Square hidden={hidden[15]} clicked={clicked[15]} performClick={() => performClick(15)} backValue={values[15]}/>
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Square = ({hidden=false, clicked=false, performClick}) => {
+const Square = ({ hidden=false, clicked=false, performClick, backValue }) => {
 
   if(hidden) return(
     <div className="w-16 h-16 m-2 justify-center items-center flex"></div>
@@ -11,7 +11,7 @@ const Square = ({hidden=false, clicked=false, performClick}) => {
       className="w-16 h-16 m-2 justify-center items-center flex cursor-pointer border-2 text-white"
       onClick={performClick}
     >
-      {clicked ? "front" : "back"}
+      {clicked ? backValue : "front" }
       {/*<section class="w-16 h-16 m-2 justify-center items-center flex cursor-pointer text-white">
         <div class="w-16 h-16 bg-transparent cursor-pointer group perspective">
           <div class="preserve-3d group-hover:rotate-y-180 w-full h-full duration-1000 border-2">
